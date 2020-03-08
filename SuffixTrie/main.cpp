7bc8afb2646a03319b4fae26ce7002fbdb5bba13
Adeeb Mohammed <adeeb2358@gmail.com>
2020-03-09 00:15:55 +0900

    Initial Commit

---
+++ SuffixTrie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuffixTrie
+//
+//  Created by adeeb mohammed on 09/03/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
